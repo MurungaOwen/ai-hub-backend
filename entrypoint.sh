b3b8ad2856846f537c7b090eaf8aa4380cdddf05
Murunga <122882250+MurungaOwen@users.noreply.github.com>
2024-11-30 10:27:11 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,28 +3,16 @@
 # Apply database migrations
 echo "Applying database migrations..."
 python manage.py makemigrations
-
-echo "migrating..."
 python manage.py migrate
 
-# Run seeders (or any custom commands)
-echo "Running seeders..."
-python manage.py seed_courses
-
-echo "Creating admin user"
-python manage.py create_admin
-
-echo "seeding forums"
-python manage.py add_forums
-
-# Collect static files (for production)
+# Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Start cron service
+# Start the cron service
 echo "Starting cron service..."
 service cron start
 
-# Start Gunicorn server
+
 echo "Starting Gunicorn server..."
-exec gunicorn ai_hub.wsgi:application --bind 0.0.0.0:8000
+exec "$@"
