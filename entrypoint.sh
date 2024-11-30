00e6369abc76bca7f6ee931d3229a3e1bb3290a3
Murunga <122882250+MurungaOwen@users.noreply.github.com>
2024-11-30 10:35:20 +0300

    add custom commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,19 @@ python manage.py migrate
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Seeding courses.."
+python manage.py seed_courses
+
+echo "Adding forums..."
+python manage.py add_forums
+
+echo "fetch ai news.."
+python manage.py fetch_news
+
+echo "Creating admin .."
+python manage.py create_admin
+
+
 # Start the cron service
 echo "Starting cron service..."
 service cron start
